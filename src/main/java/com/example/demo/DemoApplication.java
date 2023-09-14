2cca1615c9c98fc46323911d5eead5b74d811115
miles.lee <miles.lee@nsuslab.com>
2023-09-14 14:37:38 +0900

    feat: add repository

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,48 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 
 @SpringBootApplication
 public class DemoApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
 
+@Entity 
 class Coffee {
-	private final String id;
+	@Id 
+	private String id;
 	private String name;
+
+	// No args Constructor(기본 생성자) 메서드. 기본 생성자란 파라미터가 없는 생성자를 말함.
+	// JPA를 사용할때에는 기본 생성자(no-argument)가 필요함. 
+	public Coffee() {
+	}
 
 	// Constructor 메서드 : 객체를 생성하는 역할을 함. 생성된 객체를 반환. new 키워드와 함께 클래스를 생성할때 자동을 호출됨.
 	// 이름이 Class의 이름과 동일함. 반환값을 명시하지 않음에도 void를 선언하지 않음.
@@ -35,6 +54,11 @@ class Coffee {
 	// Constructor 메서드 
 	public Coffee(String name) {
 		this(UUID.randomUUID().toString(), name);
+	}
+
+	// Setter 메서드
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	// Getter 메서드
@@ -53,63 +77,64 @@ class Coffee {
 	}
 }
 
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
 @RestController
 @RequestMapping("/coffees") // 이 Annotation을 통해 각 메소드마다 반복되는 URI를 통합할 수 있다.
 class RestApiDemoController {
-	private List<Coffee> coffees = new ArrayList<>();
 
-	public RestApiDemoController() {
-		coffees.addAll(List.of(
-						new Coffee("Cafe Cereza"),
-						new Coffee("Cafe Ganador"),
-						new Coffee("Cafe Lareno"),
-						new Coffee("Cafe Tres Pontas")
-		));
+	private final CoffeeRepository coffeeRepository;
+	
+	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
 	}
 
 	@GetMapping
 	Iterable<Coffee> getCoffees() { //Iterable<Coffee>는 getCoffees() 메소드의 반환 타입을 나타냄. Iterable은 반복 가능한 Java의 인터페이스.
-		return coffees;
+		return coffeeRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) { //Optional은 생략 가능한 값을 나타내는 Java 클래스.
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				return Optional.of(c); //Optional.of()는 매개변수를 Optional 클래스로 감싸는 역할을 함.
-			}
-		}
-
-		return Optional.empty();
+		return coffeeRepository.findById(id);
 	}
 
 	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
+		return coffeeRepository.save(coffee);
 	}
 
 	@PutMapping("/{id}") // Put 메서드는 상태코드 반환이 필수. 그래서 상태 코드를 포함하는 ResponseEntity 클래스 객체를 반환한다.
 	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
-
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-
-		return (coffeeIndex == -1) ? 
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) : 
-				new ResponseEntity<>(coffee, HttpStatus.OK);
+		return (coffeeRepository.existsById(id))
+			   	? new ResponseEntity<>(coffeeRepository.save(coffee),HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee),HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
-		coffees.removeIf(c -> c.getId().equals(id)); // removeIf 메서드는 List의 각 요소에 대해 Predicate 함수를 호출하여 조건을 검사함. 조건이 True인 요소를 발견할시 이를 삭제함.
-													 // Predicate란 Java에서 함수형 프로그래밍을 지원하는 인터페이스. 하나의 인자를 받아서 true, 또는 false를 반환함.
+		coffeeRepository.deleteById(id);
+		// coffees.removeIf(c -> c.getId().equals(id)); // removeIf 메서드는 List의 각 요소에 대해 Predicate 함수를 호출하여 조건을 검사함. 조건이 True인 요소를 발견할시 이를 삭제함.
+		// 											 // Predicate란 Java에서 함수형 프로그래밍을 지원하는 인터페이스. 하나의 인자를 받아서 true, 또는 false를 반환함.
+	}
+}
+
+// 데이터 생성 로직의 분리를 위해 별도 class를 생성.
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
 	}
 
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
+			new Coffee("Cafe Cereza"),
+			new Coffee("Cafe Ganador"),
+			new Coffee("Cafe Lareno"),
+			new Coffee("Cafe Tres Pontas")
+		));
+	}
 }
 
